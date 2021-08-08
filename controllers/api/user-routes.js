57f97226de2b2c8566264f1c925c45505200cbb5
Victor Lupascu <sinajeen@gmail.com>
2021-08-08 17:03:56 -0500

    add delete user route

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -35,4 +35,24 @@ router.post('/', (req, res) => {
         });
 });
 
+// delete an user
+router.delete('/:id', (req, res) => {
+    User.destroy({
+        where: {
+            id: req.params.id
+        }
+    })
+        .then(dbUserData => {
+            if (!dbUserData) {
+                res.status(404).json({ message: 'No user found with this id' });
+                return;
+            }
+            res.json(dbUserData);
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        });
+});
+
 module.exports = router;
