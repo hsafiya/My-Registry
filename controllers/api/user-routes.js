d90d783f51502af5fcac5c62713e88b7982db1c5
Victor Lupascu <sinajeen@gmail.com>
2021-08-08 17:00:43 -0500

    add create user route

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -4,7 +4,8 @@ const { User } = require('../../models');
 // get all users
 router.get('/', (req, res) => {
     User.findAll({
-        attributes: { exclude: ['password'] }
+        // will uncomment this for deployed app
+        // attributes: { exclude: ['password'] }
     })
         .then(dbUserData => res.json(dbUserData))
         .catch(err => {
@@ -13,5 +14,25 @@ router.get('/', (req, res) => {
         });
 });
 
+// add a user
+router.post('/', (req, res) => {
+    // expects {username: 'sinajeen', email: 'sinajeen@gmail.com', password: 'password1234'}
+    // dummy data to use in insomnia
+    // {
+    //     "username": "sinajeen",
+    //     "email": "sinajeen@gmail.com",
+    //     "password": "password1234"
+    // }
+    User.create({
+        username: req.body.username,
+        email: req.body.email,
+        password: req.body.password
+    })
+        .then(dbUserData => res.json(dbUserData))
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        });
+});
 
 module.exports = router;
