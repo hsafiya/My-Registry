a98c0a54441df2fa6f5529acd1834c141ef495d0
Victor Lupascu <sinajeen@gmail.com>
2021-08-08 16:47:14 -0500

    add user model, get all users route and adjust the CMV structure

---
+++ controllers/api/user-routes.js
@@ -0,0 +1,17 @@
+const router = require('express').Router();
+const { User } = require('../../models');
+
+// get all users
+router.get('/', (req, res) => {
+    User.findAll({
+        attributes: { exclude: ['password'] }
+    })
+        .then(dbUserData => res.json(dbUserData))
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        });
+});
+
+
+module.exports = router;
---
+++ server.js
@@ -0,0 +1,19 @@
+// import  general dependencies
+const express = require('express');
+const sequelize = require("./config/connection");
+
+// assign variables to start express and set the port
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+// add midlleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+// set up the routes
+app.use(require('./controllers/'));
+
+// sync sequelize with the server
+sequelize.sync({ force: false }).then(() => {
+    app.listen(PORT, () => console.log('Now listening'));
+});
