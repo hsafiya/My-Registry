f05a9c878dad3612697443a4793e46de8a4a1e3d
Victor Lupascu <sinajeen@gmail.com>
2021-08-08 23:26:13 -0500

    add category delete route

--- controllers/api/categories-routes.js
+++ controllers/api/categories-routes.js
@@ -52,7 +52,25 @@ router.put('/:id', (req, res) => {
         });
 });
 
-
+// delete a category by its `id` value
+router.delete('/:id', (req, res) => {
+    Category.destroy({
+        where: {
+            id: req.params.id
+        }
+    })
+        .then(dbData => {
+            if (!dbData) {
+                res.status(404).json({ message: 'No category found with this id' });
+                return;
+            }
+            res.json(dbData);
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        });
+});
 
 
 module.exports = router;
