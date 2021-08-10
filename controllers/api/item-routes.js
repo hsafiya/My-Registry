3eb7839703f8f41213b8b029c7282245e7f6f512
AlexN <82428461+anaumov1@users.noreply.github.com>
2021-08-10 17:56:25 -0500

    get by id item-routes

--- controllers/api/item-routes.js
+++ controllers/api/item-routes.js
@@ -7,11 +7,11 @@ const { Registry, Category, Item, User, RegistryCategories } = require('../../mo
 router.get('/', (req, res) => {
     // find all tags
     Item.findAll({
-        attributes: ['id', 'item_name', 'item_url'],
+        attributes: ['id', 'item_name', 'item_url', 'bought', 'registry_id'],
         include: [
             {
                 model: Registry,
-                attributes: ['id', 'title']
+                attributes: ['title']
             },
            
         ]
@@ -22,6 +22,37 @@ router.get('/', (req, res) => {
             res.status(500).json(err);
         });
 });
+
+
+// find one registry by id, owner, category and ite items
+router.get('/:id', (req, res) => {
+    Item.findOne({
+        where: {
+            id: req.params.id
+        },
+        attributes: ['id', 'item_name', 'item_url', 'bought', 'registry_id'],
+        include: [
+            {
+                model: Registry,
+                attributes: ['title']
+            },
+        ]
+    })
+        .then(dbItemData => {
+            if (!dbItemData) {
+                res.status(404).json({ message: 'No registry found with this id' });
+                return;
+            }
+            res.json(dbItemData);
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        });
+});
+
+
+
 
 
 module.exports = router;
