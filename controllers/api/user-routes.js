8f5d95874bbaa2ef44dd0fdd4ef9854e71dc3a69
Victor Lupascu <sinajeen@gmail.com>
2021-08-11 11:56:58 -0500

    add logout route

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -139,6 +139,18 @@ router.post('/login', (req, res) => {
     });
 });
 
+// logout route
+router.post('/logout', (req, res) => {
+    if (req.session.loggedIn) {
+        req.session.destroy(() => {
+            res.status(204).end();
+        });
+    }
+    else {
+        res.status(404).end();
+    }
+});
+
 // delete an user
 router.delete('/:id', (req, res) => {
     User.destroy({
