6468a37f193aac6f6b445fefecf696a40426e685
Victor Lupascu <sinajeen@gmail.com>
2021-08-11 11:56:16 -0500

    add login route

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -109,6 +109,36 @@ router.put('/:id', (req, res) => {
         });
 });
 
+// login route
+router.post('/login', (req, res) => {
+    // expects {username: 'sinajeen', password: 'password1234'}
+    User.findOne({
+        where: {
+            username: req.body.username
+        }
+    }).then(dbUserData => {
+        if (!dbUserData) {
+            res.status(400).json({ message: 'Incorrect username!' });
+            return;
+        }
+
+        const validPassword = dbUserData.checkPassword(req.body.password);
+
+        if (!validPassword) {
+            res.status(400).json({ message: 'Incorrect password!' });
+            return;
+        }
+
+        req.session.save(() => {
+            req.session.user_id = dbUserData.id;
+            req.session.username = dbUserData.username;
+            req.session.loggedIn = true;
+
+            res.json({ user: dbUserData, message: 'You are now logged in!' });
+        });
+    });
+});
+
 // delete an user
 router.delete('/:id', (req, res) => {
     User.destroy({
