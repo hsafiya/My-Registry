6da9eeee20c657dcc5e57a4cafbf4bc2848339cd
Iryna <iryna.shulim@gmail.com>
2021-08-10 23:04:36 -0500

    function for image slideshow

--- public/javascript/script.js
+++ public/javascript/script.js
@@ -1,14 +1,9 @@
-var myIndex = 0;
-carousel();
 
-function carousel() {
-  var i;
-  var x = document.getElementsByClassName("mySlides");
-  for (i = 0; i < x.length; i++) {
-    x[i].style.display = "none";  
-  }
-  myIndex++;
-  if (myIndex > x.length) {myIndex = 1}    
-  x[myIndex-1].style.display = "block";  
-  setTimeout(carousel, 3000); // Change image every 2 seconds
-}
+//hero sectiom images
+$('.banner-area').slick({
+  autoplay: true,
+  speed: 2000,
+  arrows: false,
+  dots: true,
+  fade: true
+});
