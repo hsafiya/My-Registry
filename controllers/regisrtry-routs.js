42f1d9b24629d527ebe975f786585e19c56c2130
Safiyya H <safiyya.hasanova@gmail.com>
2021-08-11 19:28:05 -0500

    1st rout get all reg

--- controllers/regisrtry-routs.js
+++ controllers/regisrtry-routs.js
@@ -1,6 +1,14 @@
 const router = require ('express').Router();
 
-const {User,Category, Registry, Item} = require ('../models')
-
+const {User,Category, Registry, Item, RegistryCategories} = require ('../models')
+// get all registries
+router.get('/', (req,res) => {
+    Registry.findAll({
+        include:[{
+            model:User,
+            attributes:['id','username']
+        }]
+    })
+})
 
 module.exports = router;
