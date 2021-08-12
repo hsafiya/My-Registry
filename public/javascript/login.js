8138787671bffeb42655c1cec3c1fb0ab8aa47d0
AlexN <82428461+anaumov1@users.noreply.github.com>
2021-08-12 17:02:00 -0500

    partial navigation

--- public/javascript/login.js
+++ public/javascript/login.js
@@ -18,6 +18,7 @@ async function loginFormHandler(event) {
             alert(response.statusText);
         }
     }
+    document.location.reload
 };
 
 document.querySelector('#login').addEventListener('click', loginFormHandler);
