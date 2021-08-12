32ecddd38e62c7539687fdcdbb030bbd55b4a870
AlexN <82428461+anaumov1@users.noreply.github.com>
2021-08-12 17:46:20 -0500

    debug

--- public/javascript/redirect.js
+++ public/javascript/redirect.js
@@ -1,10 +1,10 @@
-document.getElementById('login-redirect').onclick = function (event) {
+document.getElementById('login-redirect').addEventListener('click', function (event) {
     event.preventDefault();
     document.location.replace('/login/')
-  };
+  });
   
-  document.getElementById('signup-redirect').onclick = function (event) {
+  document.getElementById('signup-redirect').addEventListener('click', function (event) {
     event.preventDefault();
     document.location.replace('/signup/')
-  };
+  });
   
