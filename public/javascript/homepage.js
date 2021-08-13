0b48b5b9c893195ab256a3d607ca10670409a377
Iryna <iryna.shulim@gmail.com>
2021-08-13 15:38:17 -0500

    changing function

--- public/javascript/homepage.js
+++ public/javascript/homepage.js
@@ -3,11 +3,17 @@ function viewAllRegistriesHandler(event) {
     document.location.replace('/registries/')   
 };
 
-function createRegistryHandler(event) {
-    event.preventDefault();
-    document.location.replace('/choosecategory/')   
-};
+// function createRegistryHandler(event) {
+//     event.preventDefault();
+//     document.location.replace('/choosecategory/')   
+// };
 
 
 document.querySelector('#allregs').addEventListener('click', viewAllRegistriesHandler);
-document.getElementById("createregistry").addEventListener('click', createRegistryHandler);
+//document.getElementById("createregistry").addEventListener('click', createRegistryHandler);
+
+
+document.getElementById('createregistry').onclick = function (event) {
+    event.preventDefault();
+    document.location.replace('/choosecategory/')
+};
