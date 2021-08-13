24f40e7d9f215c3ff664fed226c4276668e01f11
Iryna <iryna.shulim@gmail.com>
2021-08-12 20:14:50 -0500

    redirecting from choose category to create registry function

---
+++ public/javascript/choosecategory.js
@@ -0,0 +1,5 @@
+// redirect to create registry page
+document.querySelector(".card-image").onclick = function (event) {
+    event.preventDefault();
+    document.location.replace("/createregistry/")
+};
