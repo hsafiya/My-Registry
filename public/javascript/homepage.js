3cf40129cd45d79722b00d0ac1a279830ee5bb29
Iryna <iryna.shulim@gmail.com>
2021-08-13 15:00:50 -0500

    adding function to redirect to registries list on click

--- public/javascript/homepage.js
+++ public/javascript/homepage.js
@@ -1,2 +1,7 @@
+function viewAllRegistriesHandler(event) {
+    event.preventDefault();
+    document.location.replace('/registries/')   
+};
 
+document.querySelector('#allregs').addEventListener('click', viewAllRegistriesHandler);
 
