4ccd2aa052fb28bfe533451509c30b3f2766ba74
Safiyya H <safiyya.hasanova@gmail.com>
2021-08-14 17:14:04 -0500

    filter cat name

--- controllers/regisrtry-routs.js
+++ controllers/regisrtry-routs.js
@@ -24,3 +24,30 @@ router.get('/', (req, res) => {
         res.status(500).json(err);
     })
 });
+
+// filter by category name
+// wedding
+router.get('/wedding-registries', (req, res) => {
+    Registry.findAll({
+        include: [
+            {
+                model: User,
+                attributes: ['id', 'username']
+            },
+            {
+                model: Category
+            },
+            {
+                model: Item
+            }
+        ]
+    }).then(dbRegistryData => {
+        const registries = dbRegistryData.map(post => post.get({ plain: true }));
+        const catName = registries.filter(cat => cat.categories[0].category_name === 'wedding');
+
+        res.render('wed-regs', { catName })
+    }).catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    })
+});
