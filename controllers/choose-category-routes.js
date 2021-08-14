a0d6e6369d35f08f0a72f8161146f0902789e076
Iryna <iryna.shulim@gmail.com>
2021-08-13 22:42:25 -0500

    adding route for choose-category

---
+++ controllers/choose-category-routes.js
@@ -0,0 +1,16 @@
+const router = require('express').Router();
+
+const { User, Category, Registry, Item, RegistryCategories } = require('../models')
+//get all categories
+router.get('/', (req, res) => {
+    Category.findAll({
+
+    }).then(dbCategoryData => {
+        const chooseCategory = dbCategoryData.map(post => post.get({ plain: true })) 
+        res.render('choose-category', { chooseCategory })
+    }).catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
+});
+    module.exports = router;
