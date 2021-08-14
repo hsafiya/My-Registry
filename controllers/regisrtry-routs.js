609b880aa15baaef9cff1792175a64656c897832
Safiyya H <safiyya.hasanova@gmail.com>
2021-08-14 17:13:34 -0500

    reg routs 1

--- controllers/regisrtry-routs.js
+++ controllers/regisrtry-routs.js
@@ -4,57 +4,23 @@ const { User, Category, Registry, Item, RegistryCategories } = require('../model
 // get all registries
 router.get('/', (req, res) => {
     Registry.findAll({
-        include: [{
-            model: User,
-            attributes: ['id', 'username']
-        },
-        {
-            model: Category
-        },
-        {
-            model: Item
-        }
+        include: [
+            {
+                model: User,
+                attributes: ['id', 'username']
+            },
+            {
+                model: Category
+            },
+            {
+                model: Item
+            }
         ]
     }).then(dbRegistryData => {
         const registries = dbRegistryData.map(post => post.get({ plain: true }))
-        res.render('registries', {
-            logged: req.session.logged,
-            registries
-        })
+        res.render('registries', { registries })
     }).catch(err => {
         console.log(err);
         res.status(500).json(err);
     })
-
-    // get a registry by title
-    router.get('/:title', (req, res) => {
-        Registry.findOne({
-            where: {
-                title: req.params.title
-            },
-            include: [
-                {
-                    model: User,
-                    attributes: ['id', 'username']
-                },
-                {
-                    model: Category
-                }
-            ]
-        }).then(dbRegistryData => {
-            const registry = dbRegistryData.get({ plain: true });
-            res.render('registries', {
-                logged: req.session.logged,
-                registry
-            });
-        }).catch(err => {
-            console.log(err);
-            res.status(500).json(err);
-        })
-    });
-
-
-})
-
-
-module.exports = router;
+});
