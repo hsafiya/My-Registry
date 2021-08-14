13a13fb04ab5c187cdf8dd2867f4e81fc0ae0266
AlexN <82428461+anaumov1@users.noreply.github.com>
2021-08-13 23:37:06 -0500

    view about.handlebars function

--- public/javascript/homepage.js
+++ public/javascript/homepage.js
@@ -8,6 +8,11 @@ function createRegistryHandler(event) {
     document.location.replace('/choosecategory/')   
 };
 
+function viewAboutHandler(event) {
+    event.preventDefault();
+    document.location.replace('/about/')   
+};
+
 
 document.querySelector('.allregs1').addEventListener('click', viewAllRegistriesHandler);
 document.querySelector('.allregs2').addEventListener('click', viewAllRegistriesHandler);
@@ -18,3 +23,5 @@ document.querySelector(".createregistry1").addEventListener('click', createRegis
 document.querySelector(".createregistry2").addEventListener('click', createRegistryHandler);
 document.querySelector(".createregistry3").addEventListener('click', createRegistryHandler);
 document.querySelector(".createregistry4").addEventListener('click', createRegistryHandler);
+
+document.querySelector("#about").addEventListener('click', viewAboutHandler);
