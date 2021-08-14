95a9fb34933a100dc37e75e9ed356f1673924961
Safiyya H <safiyya.hasanova@gmail.com>
2021-08-14 17:17:35 -0500

    reg name

---
+++ public/js/registries-page.js
@@ -0,0 +1,8 @@
+// find registry name
+async function regSearchHandler(event){
+    event.preventDefault();
+    const registryName = document.querySelector('#reg-search').value.trim();
+    if (registryName) {
+       document.location.replace(`/registries/${registryName}`)
+    }
+};
