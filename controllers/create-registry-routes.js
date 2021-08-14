301dd9797b366e968609fefee02ce9caa711306e
Iryna <iryna.shulim@gmail.com>
2021-08-14 12:04:37 -0500

    adding route to create-registry page

---
+++ controllers/create-registry-routes.js
@@ -0,0 +1,14 @@
+const router = require('express').Router();
+const withAuth = require('../utils/auth');
+
+router.get('/',withAuth, (req, res) => {
+    if(req.session.logged) {
+        res.render('create-registry', {                
+            logged: req.session.logged
+        })
+        return;
+    };
+    res.render('homepage')
+});
+
+module.exports = router;
