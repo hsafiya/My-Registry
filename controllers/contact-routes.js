60a3ab4361404794017ad8f63b9219d48e4a38c2
AlexN <82428461+anaumov1@users.noreply.github.com>
2021-08-15 10:53:52 -0500

    router.get function

--- controllers/contact-routes.js
+++ controllers/contact-routes.js
@@ -1,4 +1,13 @@
 const router = require('express').Router();
 
+router.get('/', (req, res) => {
+    if (req.session.logged) {
+        res.render('contact', {
+            logged: req.session.logged
+        })
+        return;
+    };
+    res.render('contact')
+})
 
 module.exports = router;
