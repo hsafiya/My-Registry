b855a6363093b5faf64367a1e6007fad63b8a3f2
Victor Lupascu <sinajeen@gmail.com>
2021-08-16 23:35:20 -0500

    edit profile routes, handlebars

--- controllers/profile-routes.js
+++ controllers/profile-routes.js
@@ -1,13 +1,26 @@
+const { User } = require('../models');
+
 const router = require('express').Router();
 
 router.get('/', (req, res) => {
-    if (req.session.logged) {
-        res.render('profile', {
-            logged: req.session.logged
-        })
-        return;
-    };
-    res.render('login')
+    User.findOne({
+        where: {
+            id: req.session.user_id
+        }
+    }).then(dbUserData => {
+        const user = dbUserData.get({plain:true});
+        if (req.session.logged) {
+            res.render('profile', {
+                user,
+                logged: req.session.logged
+            })
+            return;
+        };
+        res.render('login')
+    }).catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    })    
 })
 
 module.exports = router;
---
+++ public/javascript/profile.js
@@ -0,0 +1,12 @@
+const updateUsername = document.querySelector('#username-update');
+const updateEmail = document.querySelector('email-update');
+const updatePassword = document.querySelector('password-update');
+
+const updateFormBtn = document.querySelector('#submit-profile');
+const deleteProfile = document.querySelector('#delete-profile');
+
+updateFormBtn.addEventListener('click', (event) => {
+    event.preventDefault();
+
+
+})
